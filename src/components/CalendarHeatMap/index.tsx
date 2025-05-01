3dd4a6caec546ba16f8f7a4fdf8b90e180c152e4
krislorem <2145619745@qq.com>
2025-05-01 22:31:41 +0800

    add: Calendar Contributes HeatMap

--- src/components/CalendarHeatMap/index.tsx
+++ src/components/CalendarHeatMap/index.tsx
@@ -5,20 +5,27 @@ import type { Activity } from 'react-activity-calendar'
 import React from 'react'
 import './index.css'
 const CalendarHeatMap: React.FC<{ data: Activity[] }> = ({ data }) => {
+  const customTheme = {
+    light: ['hsl(0,0%,92%)', '#0ac740']
+  }
   return (
-    <>
+    <div className='heatmap'>
       <ActivityCalendar
         showWeekdayLabels
         data={data}
+        theme={customTheme}
+        labels={{
+          totalCount: '{{year}} 年有 {{count}} 贡献'
+        }}
         renderBlock={(block, activity) =>
           React.cloneElement(block, {
             'data-tooltip-id': 'react-tooltip',
-            'data-tooltip-html': `${activity.count} activities on ${activity.date}`,
+            'data-tooltip-html': `${activity.count} 贡献 ${activity.date}`,
           })
         }
       />
       <ReactTooltip id="react-tooltip" />
-    </>
+    </div>
   )
 }
 
